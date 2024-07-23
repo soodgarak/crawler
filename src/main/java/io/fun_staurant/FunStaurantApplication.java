370e5ab0e483f40b9c8bac93d27320f1a631e9a0
ndy <ratcomp9992@gmail.com>
2024-07-24 08:19:54 +0900

    Init: initial project

---
+++ src/main/java/io/fun_staurant/FunStaurantApplication.java
@@ -0,0 +1,13 @@
+package io.fun_staurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunStaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunStaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/fun_staurant/FunStaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fun_staurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunStaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
