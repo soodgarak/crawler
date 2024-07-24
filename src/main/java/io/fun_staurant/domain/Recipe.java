c3f3aa14593f0fdf041862e9d4ee4b0554d803ed
ndy <ratcomp9992@gmail.com>
2024-07-25 06:12:40 +0900

    Feat: entity 추가

---
+++ src/main/java/io/fun_staurant/domain/Recipe.java
@@ -0,0 +1,34 @@
+package io.fun_staurant.domain;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @Column(nullable = false)
+    private String description;
+
+    private String imageURL;
+
+    private Recipe (String title, String contents, String description, String imageURL) {
+        this.title = title;
+        this.contents = contents;
+        this.description = description;
+        this.imageURL = imageURL;
+    }
+
+    public static Recipe of(String title, String contents, String description, String imageURL) {
+        return new Recipe(title, contents, description, imageURL);
+    }
+}
